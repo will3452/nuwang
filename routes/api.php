f3021cecdada7dc21a32f8d8ae7a516dbf8ab4f7
will3452 <36730333+will3452@users.noreply.github.com>
2021-10-18 18:13:35 +0800

    lupet

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/covid', function () {
+    $client = new Client();
+    $crawler = $client->request('GET', 'http://nuwang.tech/users/login');
+    dd($crawler);
 });
