25f88fed825374fc44f6963dde68753617becf24
William Galas <williamgalas2@gmail.com>
2022-01-21 23:38:27 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,6 +122,20 @@
         </main>
     </div>
 
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/61ead2d69bd1f31184d89bf3/1fpukf6en';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+
     <script>
          const hamburger = document.querySelector(".hamburger");
         const navMenu = document.querySelector(".nav-menu");
